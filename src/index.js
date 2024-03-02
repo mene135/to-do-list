74f5f0e97f9fd22b1cc6d88f1fbec61688041fad
mene13 <mene13@Xubuntu.myguest.virtualbox.org>
2024-03-02 18:48:05 +0200

    added accesibility navigation

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ toggleThemeBtn.addEventListener("click", () => {
 const menuToggleBtn = document.querySelector(".menuToggle");
 
 menuToggleBtn.addEventListener("click", () => {
+    const ariaExpandedValue = menuToggleBtn.getAttribute("aria-expanded");
+    if(ariaExpandedValue === "true") {
+        menuToggleBtn.setAttribute("aria-expanded", "false");
+    } else {
+        menuToggleBtn.setAttribute("aria-expanded", "true");
+    }
+
+
     const nav = document.querySelector(".l-mainNav");
     console.log("hi i am menelaos");
 
