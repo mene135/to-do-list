7afc80b181796b6f0e54dbcb8215728a4b215d7c
mene13 <mene13@Xubuntu.myguest.virtualbox.org>
2024-03-06 19:22:56 +0200

    added tab functionality as well as active state for those tabs

--- src/index.js
+++ src/index.js
@@ -43,6 +43,83 @@ function isNavExpanded() {
     }
 }
 
+class Tab {
+    constructor(tabName, tabToDos) {
+        this.tabName = tabName;
+        this.tabToDos = tabToDos;
+    }
+
+    click = () => {
+        const main = document.querySelector("main");
+
+        const h3 = document.createElement("h3");
+        h3.textContent = this.tabName;
+
+        main.appendChild(h3);
+    }
+
+    appendToDo = (toDo) => {
+        this.tabToDos.push(toDo);
+    }
+} 
+
+const tabs = document.querySelectorAll(".tab");
+const allTasks = new Tab(tabs[0].childNodes[1].textContent, []);
+const today = new Tab(tabs[1].childNodes[1].textContent, []);
+const next7Days = new Tab(tabs[2].childNodes[1].textContent, []);
+const important = new Tab(tabs[3].childNodes[1].textContent, []);
+
+const homeSectionTabs = [];
+homeSectionTabs.push(allTasks);
+homeSectionTabs.push(today);
+homeSectionTabs.push(next7Days);
+homeSectionTabs.push(important);
+
+console.log(next7Days);
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        clearActive(tab)
+        clear();
+        click(tab.childNodes[1].textContent);
+        tab.classList.add("active");
+    })
+})
+
+function click(tabClicked) {
+    const main = document.querySelector("main");
+
+    const div = document.createElement("div");
+    div.classList.add("title");
+
+    const h3 = document.createElement("h3");
+    h3.textContent = tabClicked;
+    h3.classList.add("title-heading");
+
+    div.appendChild(h3);
+    main.appendChild(div);
+}
+
+function clear() {
+    const main = document.querySelector("main");
+
+    main.innerHTML = "";
+}
+
+function clearActive(tabClicked) {
+    tabs.forEach(tab => {
+        if(tabClicked !== tab) {
+            tab.classList.remove("active")
+        }
+    })
+}
+
+
+
 window.onload = () => {
+
+    const tabs = document.querySelectorAll(".tab");
+
+    console.log(tabs[3].childNodes);
+
     isNavExpanded();
 }
